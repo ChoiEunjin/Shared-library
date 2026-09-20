ca4d6a6453fdba80886555c1dd96a85685e62e13
ChoiEunjin <diliah0826@gmail.com>
2026-09-20 21:46:18 +0900

    Update pipelineStages.groovy

--- vars/pipelineStages.groovy
+++ vars/pipelineStages.groovy
@@ -30,18 +30,19 @@ def call() {
                 }
             }
 
-            stage('SonarQube Analysis') {
-                environment {
-                    scannerHome = tool 'sonar'
-                }
-                steps {
-                    script {
-                        withSonarQubeEnv(credentialsId: 'sonar_token', installationName: 'sonar') {
-                            sh "'''${scannerHome}/bin/sonar-scanner''' -Dsonar.projectKey=${REPO_NAME} -Dsonar.java.binaries=. -Dsonar.projectBaseDir=${WORKSPACE}"
-                        }
-                    }
-                }
+stage('SonarQube Analysis') {
+    environment {
+        scannerHome = tool 'sonar'
+    }
+    steps {
+        script {
+            withSonarQubeEnv(credentialsId: 'sonar_token', installationName: 'sonar') {
+                def scannerExec = "${scannerHome}/bin/sonar-scanner"
+                sh "'${scannerExec}' -Dsonar.projectKey=${env.REPO_NAME} -Dsonar.java.binaries=. -Dsonar.projectBaseDir='${env.WORKSPACE}' -Dsonar.exclusions='**/*.js,**/*.ts,**/*.css'"
             }
+        }
+    }
+}
         }
     }
 }
